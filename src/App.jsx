5af9d5455edc0e0f4c7c45051060ecf3c5e97a48
Damian <mlodybarney@gmail.com>
2019-06-01 19:31:34 +0200

    Add Eslint and refactor code according to Eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,45 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "./App.css";
-import Countdown from "./Countdown";
-import EditEvent from "./Editevent";
+import Countdown from "./Countdown.jsx";
+import EditEvent from "./Editevent.jsx";
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      events: [
-        { id: 0, name: "Event 1", hour: "14", minute: "7", },
-        { id: 1, name: "Event 2", hour: "03", minute: "15" },
-        { id: 2, name: "Event 3", hour: "13", minute: "22" }
-      ],
-      editedEvent: {
-        id: 3, name: "", hour: "", minute: ""
-      }
-    };
+	constructor() {
+		super();
+		this.state = {
+			events: [
+				{ id: 0, name: "Event 1", hour: "14", minute: "07", },
+				{ id: 1, name: "Event 2", hour: "03", minute: "15" },
+				{ id: 2, name: "Event 3", hour: "13", minute: "22" }
+			],
+			editedEvent: {
+				id: 3, name: "", hour: "", minute: ""
+			}
+		};
 
-    this.handleEditEvent = this.handleEditEvent.bind(this);
-  }
+		this.handleEditEvent = this.handleEditEvent.bind(this);
+	}
 
-  handleEditEvent(val) {
-    this.setState(prevState => {
-      return {
-        editedEvent: Object.assign(prevState.editedEvent, val)
-      }
-    })
-  }
+	handleEditEvent(val) {
+		this.setState(prevState => {
+			return {
+				editedEvent: Object.assign(prevState.editedEvent, val)
+			};
+		});
+	}
 
-  render() {
-    const events = this.state.events.map(el => {
-      return <Countdown key={el.id} name={el.name} hour={el.hour} minute={el.minute} />;
-    });
-    return <div className="app">
-      {events}
-      <EditEvent
-        onInputChange={val => this.handleEditEvent(val)}
-        onSave={() => alert("Test")}
-      />
-    </div>
-  };
+	render() {
+		const events = this.state.events.map(el => {
+			return <Countdown key={el.id} name={el.name} hour={el.hour} minute={el.minute} />;
+		});
+		return <div className="app">
+			{events}
+			<EditEvent
+				onInputChange={val => this.handleEditEvent(val)}
+				onSave={() => alert("Test")}
+			/>
+		</div>
+	}
 }
 
 export default App;
--- src/Editevent.jsx
+++ src/Editevent.jsx
@@ -1,27 +1,27 @@
-import React from 'react';
+import React from "react";
 import "./Editevent.css";
 
 const EditEvent = props => {
-  return (
-    <div className="edit-event">
-      <div className="edit-event__input-group">
-        <label htmlFor="name">Name</label>
-        <input type="text" id="name" name="name" onChange={e => props.onInputChange({ [e.target.name]: e.target.value })} />
-      </div>
-      <div className="edit-event__input-group">
-        <label htmlFor="hour">Hour</label>
-        <input type="tel" id="hour" name="hour" onChange={e => props.onInputChange({ [e.target.name]: e.target.value })} />
-      </div>
-      <div className="edit-event__input-group">
-        <label htmlFor="minute">Minute</label>
-        <input type="tel" id="minute" name="minute" onChange={e => props.onInputChange({ [e.target.name]: e.target.value })} />
-      </div>
-      <div className="edit-event__button-group">
-        <button onClick={() => props.onSave()}>OK</button>
-        <button>Cancel</button>
-      </div>
-    </div>
-  );
+	return (
+		<div className="edit-event">
+			<div className="edit-event__input-group">
+				<label htmlFor="name">Name</label>
+				<input type="text" id="name" name="name" onChange={e => props.onInputChange({ [e.target.name]: e.target.value })} />
+			</div>
+			<div className="edit-event__input-group">
+				<label htmlFor="hour">Hour</label>
+				<input type="tel" id="hour" name="hour" onChange={e => props.onInputChange({ [e.target.name]: e.target.value })} />
+			</div>
+			<div className="edit-event__input-group">
+				<label htmlFor="minute">Minute</label>
+				<input type="tel" id="minute" name="minute" onChange={e => props.onInputChange({ [e.target.name]: e.target.value })} />
+			</div>
+			<div className="edit-event__button-group">
+				<button onClick={() => props.onSave()}>OK</button>
+				<button>Cancel</button>
+			</div>
+		</div>
+	);
 };
 
 export default EditEvent; 
