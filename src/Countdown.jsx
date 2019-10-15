301d2a303012cdf4c1d89c9ae854fd0486996fb0
Damian Osiecki <osieckidamian1992@gmail.com>
2019-10-15 23:47:36 +0200

    Add countdown time to events

--- src/Countdown.jsx
+++ src/Countdown.jsx
@@ -1,25 +1,39 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { hourMinuteToSeconds, secondsToHourMinuteSeconds } from "./utils";
 
 import "./Countdown.css";
 import "./../node_modules/semantic-ui-css/semantic.css";
 
-const Countdown = props => (
-	<div className="countdown">
-		<strong>{props.name}</strong> - {props.hour}:{props.minute}
-		<div className="countdown__icons">
-			<i className="icon edit" onClick={() => props.onEditInit(props.id)}></i>
-			<i className="icon times" onClick={() => props.onRemove(props.id)}></i>
+const Countdown = props => {
+	const eventInSeconds = hourMinuteToSeconds(props.hour, props.minute);
+	const nowInSeconds = hourMinuteToSeconds(props.timeNow.hour, props.timeNow.minute) + props.timeNow.seconds;
+
+	const diffrence = (eventInSeconds - nowInSeconds);
+	const diffText = diffrence > 0 ? secondsToHourMinuteSeconds(diffrence) : "Jutro";
+
+	return (
+		<div className="countdown">
+			<strong>{props.name}</strong> - {diffText}
+			<div className="countdown__icons">
+				<i className="icon edit" onClick={() => props.onEditInit(props.id)}></i>
+				<i className="icon times" onClick={() => props.onRemove(props.id)}></i>
+			</div>
 		</div>
-	</div>
-);
+	);
+};
 
 Countdown.propTypes = {
 	name: PropTypes.string,
 	hour: PropTypes.number,
 	minute: PropTypes.number,
 	onEditInit: PropTypes.func,
-	onRemove: PropTypes.func
+	onRemove: PropTypes.func,
+	timeNow: PropTypes.shape({
+		hour: PropTypes.number,
+		minute: PropTypes.number,
+		seconds: PropTypes.number
+	})
 };
 
 export default Countdown;
